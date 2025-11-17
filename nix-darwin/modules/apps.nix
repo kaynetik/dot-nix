84a6346b80c4df3b064d90cf859548af3040b72e
kaynetik <aleksandar@nesovic.dev>
2025-11-17 08:19:10 +0100

    nix: flake update
    Signed-off-by: kaynetik <aleksandar@nesovic.dev>

--- nix-darwin/modules/apps.nix
+++ nix-darwin/modules/apps.nix
@@ -13,8 +13,6 @@
     poetry # Python dependency management
     mypy
     ruff
-    ## Crypto
-    # secp256k1
 
     # ============================================================
     # VCS
@@ -208,7 +206,6 @@
       "vlc"
       "gimp"
       "transmission"
-      "steam"
 
       # Communication & Entertainment
       "gather"
